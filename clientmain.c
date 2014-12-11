5828c51671275592adef483650d85bc0fdad751a
S-D- <daniil.s1001@gmail.com>
2014-12-11 11:00:32 +0200

    Access to remote chell is finished.

--- clientmain.c
+++ clientmain.c
@@ -3,6 +3,68 @@
 #include <gio/gio.h>
 #include "networking/connection.h"
 #include <string.h>
+#include <stdlib.h>
+
+GIOChannel* get_g_io_channel(gint fd)
+{
+#ifdef __WIN32__
+    return g_io_channel_win32_new_fd(fd);
+#elif __UNIX__
+    return g_io_channel_unix_new(fd);
+#endif
+}
+
+void writing_loop_fd(Connection* connection)
+{
+    GIOChannel* in_channel = get_g_io_channel(STDIN_FILENO);
+    gchar* message = NULL;
+    gsize length;
+    GError* error = NULL;
+    GIOStatus status;
+    status = g_io_channel_read_line (in_channel,
+                                     &message,
+                                     &length,
+                                     NULL,
+                                     &error);
+    while (status != G_IO_STATUS_EOF && status != G_IO_STATUS_ERROR) {
+        connection_send_message(connection, message, length);
+        g_free(message);
+        status = g_io_channel_read_line (in_channel,
+                                         &message,
+                                         &length,
+                                         NULL,
+                                         &error);
+    }
+    g_io_channel_unref(in_channel);
+}
+
+gpointer client_reading_loop(Connection* connection)
+{
+    size_t charsRead;
+    char* message;
+    GIOChannel* stdout_channel = get_g_io_channel(STDOUT_FILENO);
+    gsize bytes_written;
+    GError* error = NULL;
+    while(message = 
+          connection_read_message(connection, &charsRead)) {
+        g_io_channel_write_chars(stdout_channel,
+                                 message,
+                                 charsRead,
+                                 &bytes_written,
+                                 &error);
+        g_io_channel_flush(stdout_channel, &error);
+        free(message);
+    }
+    g_print("read end!\n");
+    g_io_channel_unref(stdout_channel);
+    return NULL;
+}
+
+gpointer client_writing_shell_out_loop(Connection* connection)
+{
+    writing_loop_fd(connection);
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 { 
@@ -30,13 +92,13 @@ int main(int argc, char *argv[])
     Connection connection = {
         gSockConnection
     };
-    char buf[100];
-    int msgSize;
-    do {
-        fgets (buf, 100, stdin);
-        msgSize = strlen(buf);
-        connection_send_message(&connection, buf, strlen(buf));
-    } while(msgSize != 1 && buf[0] != '\n');
+    
+    GThread* readingThread = g_thread_new(NULL, (GThreadFunc) client_reading_loop, &connection);
+    GThread* writingOutThread = g_thread_new(NULL, (GThreadFunc) client_writing_shell_out_loop, &connection);
+    g_thread_join(readingThread);
+    g_thread_join(writingOutThread);
+    g_thread_unref(readingThread);
+    g_thread_unref(writingOutThread);
     connection_close(&connection);
     g_object_unref(client);
     return 0;
--- server/server.c
+++ server/server.c
@@ -13,6 +13,15 @@ typedef struct client {
     volatile bool canceled;
 } Client;
 
+GIOChannel* get_g_io_channel(gint fd)
+{
+#ifdef __WIN32__
+    return g_io_channel_win32_new_fd(fd);
+#elif __UNIX__
+    return g_io_channel_unix_new(fd);
+#endif
+}
+
 Client* client_new(gint shell_stdin,
                    gint shell_stdout,
                    gint shell_stderr,
@@ -32,33 +41,79 @@ gboolean isAthorized(GInputStream* istream)
     
 }
 
-gpointer connection_reading_loop(Client* client)
+void writing_loop_fd(Client* client, gint fd)
 {
-    size_t charsRead;
-    while(char* message = 
+    GIOChannel* shell_out_channel = get_g_io_channel(fd);
+    gchar* message = NULL;
+    gsize length;
+    GError* error = NULL;
+    GIOStatus status;
+    status = g_io_channel_read_line (shell_out_channel,
+                                     &message,
+                                     &length,
+                                     NULL,
+                                     &error);
+    if (status == G_IO_STATUS_ERROR) {
+        g_error(error->message);
+        return;
+    }
+    while (status != G_IO_STATUS_EOF) {
+        connection_send_message(client->connection, message, length);
+        g_print("sent:%.*s", length, message);
+        g_free(message);
+        if (status == G_IO_STATUS_ERROR) {
+            g_error(error->message);
+            return;
+        }
+        status = g_io_channel_read_line (shell_out_channel,
+                                         &message,
+                                         &length,
+                                         NULL,
+                                         &error);
+    }
+    g_print("read end!\n");
+    g_io_channel_unref(shell_out_channel);
+}
+
+gpointer client_reading_loop(Client* client)
+{
+    gssize charsRead;
+    char* message;
+    GIOChannel* shell_in_channel = get_g_io_channel(client->shell_stdin);
+    gsize bytes_written;
+    GError* error = NULL;
+    while(message = 
           connection_read_message(client->connection, &charsRead)) {
-        fwrite(message, sizeof (char), 
-                     charsRead, client->shell_stdin);
+        g_print("recieved:%.*s", charsRead, message);
+        g_io_channel_write_chars(shell_in_channel,
+                                 message,
+                                 charsRead,
+                                 &bytes_written,
+                                 &error);
+        g_io_channel_flush(shell_in_channel, &error);
+        
         free(message);
     }
-    fclose(client->shell_stdin);
+    g_io_channel_unref(shell_in_channel);
     return NULL;
 }
 
-gpointer connection_writing_loop(Client* client)
+gpointer client_writing_shell_out_loop(Client* client)
 {
-    const buff_size = 50;
-    char buffer[buf_size];
-    do {
-        fgets (buffer, buf_size, client->shell_stdout);
-        int msg_len = strlen(buffer);
-        connection_send_message(client->connection, buffer, msg_len);
-        size_t charsRead;
-    } while (!feof(client->shell_stdout));
-    fclose(client->shell_stdout);
-    fclose(client->shell_stderr);
+    writing_loop_fd(client, client->shell_stdout);
     return NULL;
 }
+
+gpointer client_writing_shell_err_loop(Client* client)
+{
+    writing_loop_fd(client, client->shell_stderr);
+    return NULL;
+}
+
+//gpointer subproc_reading_loop(GSubprocess* subproc)
+//{
+//    g_test_subprocess();
+//}
 
 gpointer connectionHandler(gpointer connection)
 {
@@ -68,21 +123,25 @@ gpointer connectionHandler(gpointer connection)
     gchar* argv[] = {"bash", NULL};
     GError* error = NULL;
     gboolean success = g_spawn_async_with_pipes(".", argv, NULL, G_SPAWN_SEARCH_PATH,
-                             NULL, NULL, NULL, &shell_stdin, &shell_stdout,
-                             &shell_stderr, &error);
+                                                NULL, NULL, NULL, &shell_stdin, &shell_stdout,
+                                                &shell_stderr, &error);
     if (!success) {
         g_error(error->message);
         return NULL;
     }
     Client* client = client_new(shell_stdin, shell_stdout, shell_stderr, connection);
-    GThread* readingThread = g_thread_new(NULL, connection_reading_loop, client);
-    GThread* writingThread = g_thread_new(NULL, connection_writing_loop, client);
+    GThread* readingThread = g_thread_new(NULL, (GThreadFunc) client_reading_loop, client);
+    GThread* writingOutThread = g_thread_new(NULL, (GThreadFunc) client_writing_shell_out_loop, client);
+    GThread* writingErrThread = g_thread_new(NULL, (GThreadFunc) client_writing_shell_err_loop, client);
     g_thread_join(readingThread);
-    g_thread_join(writingThread);
+    g_thread_join(writingOutThread);
+    g_thread_join(writingErrThread);
     g_thread_unref(readingThread);
-    g_thread_unref(writingThread);
+    g_thread_unref(writingOutThread);
+    g_thread_unref(writingErrThread);
     connection_close(connection);
     free(connection);
+    free(client);
     return NULL;
 }
 
