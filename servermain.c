a999ae9bdde6ba5d398197cf31fe9abec134c2d5
S-D- <daniil.s1001@gmial.com>
2014-12-18 12:44:35 +0200

    Some tests

--- servermain.c
+++ servermain.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include "utils/usersinfo.h"
 #include "utils/usersxmlparser.h"
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
 
 #ifdef __WIN32__
 #include <windows.h>
@@ -34,6 +38,60 @@ void print_entry(gpointer key, gpointer value, gpointer user_data)
     printf("key:%s\n",key);
     printf(((UserInfo*)value)->name);
     printf(((UserInfo*)value)->password);
+}
+
+void test()
+{
+    FILE* keypair_file = fopen("keypair.pem", "r");
+    printf("reading keys...\n");
+    RSA* keypair = PEM_read_RSAPrivateKey(keypair_file, NULL, NULL, NULL);
+    if (keypair == NULL) {
+        printf("error reading\n");
+        ERR_print_errors_fp(stderr);
+        fclose(keypair_file);
+        return;
+    }
+    fclose(keypair_file);
+    FILE* pubkey_file = fopen("pubkey.pem", "w");
+    PEM_write_RSAPublicKey(pubkey_file, keypair);
+    fclose(pubkey_file);
+    pubkey_file = fopen("pubkey.pem", "r");
+    RSA* pubkey = PEM_read_RSAPublicKey(pubkey_file, NULL, NULL, NULL);
+    fclose(pubkey_file);
+    printf("keys read\n");
+    char* encrypt = (char*)malloc(RSA_size(keypair));
+    char* err = (char*)malloc(130);
+    char* msg = "hello!";
+    int enc_size;
+    printf("encrypting...\n");
+    if ((enc_size = RSA_public_encrypt(6, (unsigned char*)msg, (unsigned char*)encrypt,
+        pubkey, RSA_PKCS1_OAEP_PADDING)) < 0) {
+        printf("encrypting failed\n");
+        ERR_load_crypto_strings();
+        ERR_error_string(ERR_get_error(), err);
+        fprintf(stdin, "Error encrypting message: %s\n", err);
+        free(encrypt);
+        free(err);
+        return;
+    }
+    printf("encrypted\n");
+    printf("Encr:%.*s\n", enc_size, encrypt);
+    
+    // Decrypt it
+    printf("RSA SIZE Keypair:%u\n",RSA_size(keypair));
+    char* decrypt = (char*)malloc(RSA_size(keypair));
+    int decr_res;
+    if ((decr_res = RSA_private_decrypt(enc_size, (unsigned char*)encrypt, (unsigned char*)decrypt,
+        keypair, RSA_PKCS1_OAEP_PADDING)) < 0) {
+        ERR_load_crypto_strings();
+        ERR_error_string(ERR_get_error(), err);
+        fprintf(stdin, "Error decrypting message: %s\n", err);
+        free(decrypt);
+        free(err);
+        return;
+    }
+    printf("decr_size=%i\n", decr_res);
+    printf("Decr:%.*s\n", decr_res, decrypt);
 }
 
 int main(void)
